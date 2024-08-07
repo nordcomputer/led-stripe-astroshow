9d2f2f6096d159a62936f545fcde9b96c32fec16
Mario Maresch <13378398+nordcomputer@users.noreply.github.com>
2024-08-07 14:39:07 +0200

    - find matrix device

---
+++ devices.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+        if [[ $ID_SERIAL == *"Matrix"* ]]; then
+            echo $ID_SERIAL
+            echo "/dev/$devname"
+        fi
+    )
+done
